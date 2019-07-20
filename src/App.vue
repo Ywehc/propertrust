7c0fd04faeb948410de359b39f3a20a1cc25dc25
Chris <chris.kieran@gmail.com>
2019-07-20 15:13:25 -0400

    change bg image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img class="home-banner" alt="" src="./assets/home-banner-img.jpg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -24,5 +24,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.home-banner {
+  max-width: 400px;
 }
 </style>
