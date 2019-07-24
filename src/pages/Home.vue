cbdc0224cb4a65a9fc3988ae149cde5764c7e89b
Chris <chris.kieran@gmail.com>
2019-07-23 21:54:29 -0400

    responsive home basics

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home-container">
-      <h1>Testing Home</h1>
-    <!-- <h1>{{ msg }}</h1> -->
+      <div class="container">
+        <div class="row buying-selling">
+          <div class="col-6 buying">
+            <div class="buyer-button">
+              <h2>I am <strong>buying</strong></h2>
+              <p>a house, apartment or condo.</p>
+            </div>
+          </div>           
+          <div class="col-6 selling">
+            <div class="seller-button">
+              <h2>I am <strong>selling</strong></h2>
+              <p>a house, apartment or condo.</p>
+            </div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -24,8 +38,70 @@ $light-blue: #81c3d7;
 $mid-blue: #3a7ca5;
 $dark-blue: #2f6690;
 
-  .home-container {
-    background: $light;
-    padding-bottom: 80vh;
+h2 {
+  background: white;
+  margin: 0;
+  padding: 10px;
+}
+.home-container {
+  background: $light;
+  padding: 50px;
+}
+.buyer-button, .seller-button {
+  max-width: 400px;
+  --diameter: 100px;
+  padding: var(--diameter);
+  border: 2px solid #16425b;
+  border-radius: 50%;
+  margin: auto;
+  text-align: center;
+}
+.buyer-button p, .seller-button p {
+  background: white;
+  margin: 0;
+  padding: 10px;
+}
+@media only screen and (max-width: 992px) {
+  .buyer-button, .seller-button {
+    max-width: 300px;
+    --diameter: 75px;
+    padding: var(--diameter);
   }
+}
+@media only screen and (max-width: 768px) {
+  .buyer-button, .seller-button {
+    max-width: 250px;
+    --diameter: 50px;
+    padding: var(--diameter);
+  }
+  h2, p {
+    font-size: 95%;
+  }
+}
+@media only screen and (max-width: 576px) {
+  .buyer-button, .seller-button {
+    max-width: 220px;
+    padding: 35px;
+  }
+  h2 {
+    padding: 5px;
+  }
+}
+@media only screen and (max-width: 490px) {
+  h2 {
+    background: transparent;
+  }
+  .buyer-button, .seller-button {
+    max-width: 220px;
+    padding: 35px;
+  }
+  .buyer-button p, .seller-button p {
+    background: transparent;
+  }
+  .home-container {
+    padding: 15px;
+  }
+}
+
+
 </style>
