6e26b26a5c09c9576c6e01aee8718261ffe0254e
Chris <chris.kieran@gmail.com>
2019-08-02 21:41:23 -0400

    add selling page outline and routing

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg">
-      <a class="navbar-brand" href="#" v-for="copy in myJson" v-bind:key="copy.id"> {{ copy[0].brandname }}</a>
+      <router-link to="/" class="navbar-brand" v-for="copy in myJson" v-bind:key="copy.id"> {{ copy[0].brandname }}</router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
---
+++ src/components/SellerProgress.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <p>This will be a seller progress bar</p>
+  </div>
+</template>
+
+<script>
+
+// consider making each step its own component - constructor?
+import json from '../copy/data.json'
+import SellerProgress from './SellerProgress.vue';
+
+export default {
+  name: 'SellerProgress',
+  props: {
+    msg: String,
+  },
+  components: {
+    'SellerProgress': SellerProgress
+  },
+  data(){
+    return{
+      myJson: json
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Passion+One&display=swap');
+
+.container {
+  height: 70px;
+  margin: 20px auto;
+  background: $light; // in future use gradient to tie into progress
+  border: 2px solid $light-blue;
+  text-align: center;
+}
+
+</style>
